57c76d25aaacfd7fba80e0c457116a96a29726eb
Vishal Soni <vishal.soni@xipetech.com>
2023-07-05 17:41:31 +0530

    Update App.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/', routes);
-app.use('/v1/', routing);
+app.use('/api/v1/', routing);
 connectDB();
 
 const server  = http.createServer(app);
